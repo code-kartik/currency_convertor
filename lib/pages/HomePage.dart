7b201a8b3993c52014b327b32418f2eaf3bf055d
Kartik Arora <kartikarora23082003@gmail.com>
2023-10-23 20:01:15 +0530

    1 USD to INR - working✅

--- lib/pages/HomePage.dart
+++ lib/pages/HomePage.dart
@@ -10,8 +10,60 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: ,
+    return const SafeArea(
+      child: Scaffold(
+        backgroundColor: Colors.white,
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Card(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  ListTile(
+                    leading: Text(
+                      "🇺🇸",
+                      style: TextStyle(fontSize: 24),
+                    ),
+                    title: Text('USD'),
+                    subtitle: Text('United States Dollar'),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(16.0),
+                    child: Text(
+                      '1.00',
+                      style: TextStyle(fontSize: 36),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 2.0),
+            Card(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  ListTile(
+                    leading: Text(
+                      "🇮🇳",
+                      style: TextStyle(fontSize: 24),
+                    ),
+                    title: Text('INR'),
+                    subtitle: Text('Indian Rupees'),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(16.0),
+                    child: Text(
+                      '1.00',
+                      style: TextStyle(fontSize: 36),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
